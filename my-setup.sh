80e155901449691c02e4ce9fb3f80cd36004ff6b
SemicolonUnexpected <m.gracey1409@gmail.com>
2023-12-26 20:04:57 +0000

    Added some files and improved git config

--- my-setup.sh
+++ my-setup.sh
@@ -1,20 +1,61 @@
-#/bin/bash
+#!/bin/bash
 
-printf "######### Upgrading system ########"
+printf "\n######### Upgrading system ########\n"
 
 sudo pacman -Syu
 
-printf "######### Installing packages ########"
+printf "\n######## Setting up git #########\n"
 
-sudo pacman -S git neofetch curl neovim zsh python3 
+sudo pacman -S git
+git config --global user.email "m.gracey1409@gmail.com"
+git config --global user.name "SemicolonUnexpected"
+git config --global core.editor "nvim"
 
+printf "\n######## Generating ssh keys #########\n"
+
+ssh-keygen -t ed25519 -C "m.gracey1409@gmail.com"
+cat ~/.ssh/id_ed25519.pub
+
+printf "\n######## Testing connection to github #########\n"
+
+ssh -T git@github.com
+
+printf "\n######## Changing shell to zsh #########\n"
+
+sudo pacman -S zsh
+chsh -s /bin/zsh
+
+printf "\n######## Copying configs to ~/ #########\n"
+
+cp -t ~ .config .zshrc .zshenv .pk10.zsh .pk10-ascii-8colors.zsh -r
+
+printf "\n######## Setting up xorg and i3 #########\n"
+
+sudo pacman -S xorg xorg-xserver xorg-xinit i3-wm rofi alacritty
+
+printf "\n######### Installing packages ########\n"
+
+sudo pacman -S neofetch neovim zsh python3 tree 
 pip install yt-dlp
 
-git clone https://aur.archlinux.org/yay.git ~
-cd ~/yay
-makepkg -su
-cd ~ rm -r yay
+printf "\n######## Installing haskell #########\n"
 
-printf "######## Installing haskell #########"
-printf "######## Installing rust #########"
-printf "######## Installing python3 #########"
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+
+printf "\n######## Installing rust #########\n"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+printf "\n######## Installing yay #########\n"
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -r yay
+
+
+
+
+
+
