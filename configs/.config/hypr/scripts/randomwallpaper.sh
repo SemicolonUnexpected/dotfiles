3dcdbdf29487cbe5e97c65151207a678eee85041
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-01-10 19:03:01 +0000

    My vim config added

--- configs/.config/hypr/scripts/randomwallpaper.sh
+++ configs/.config/hypr/scripts/randomwallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 WALLPAPER=$(ls ~/.config/wallpapers/ | shuf -n 1)
-hyprctl hyprpaper wallpaper ~/.config/wallpapers/$WALLPAPER
+hyprctl hyprpaper preload "~/.config/wallpapers/$WALLPAPER"
+hyprctl hyprpaper wallpaper "eDP-1,~/.config/wallpapers/$WALLPAPER"
+echo hi >>~/test.txt
