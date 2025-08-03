966b9cbdea512eb55a9f45ca2b7e3c8593b30e24
SemicolonUnexpected <m.gracey1409@gmail.com>
2025-08-03 14:29:31 +0000

    Updated vim to not have space active in normal mode

--- nvim/.config/nvim/lua/semicolonunexpected/config/remap.lua
+++ nvim/.config/nvim/lua/semicolonunexpected/config/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ' '
 
 local km = vim.keymap
 
+km.set('n', '<Space>', '<Nop>', { noremap = true })
+
 -- Move selection in visual mode with 'J' and 'K'
 km.set('v', 'K', ":m '<-2<CR>gv=gv")
 km.set('v', 'J', ":m '>+1<cr>gv=gv")
